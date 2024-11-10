f5b2007b34702a8b50e1495480075cb0a49a864d
tspence <1933486+tspence@users.noreply.github.com>
2024-11-10 00:06:26 +0000

    # Patch notes for 121.0.4923
    These patch notes summarize the changes from version 121.0.4887.
    
    * Minor documentation changes, bugfixes, and performance improvements.

--- src/main/java/com/projectmanager/BlobRequest.java
+++ src/main/java/com/projectmanager/BlobRequest.java
@@ -148,7 +148,7 @@ public class BlobRequest {
             }
 
             request.addHeader("SdkName", "Java");
-            request.addHeader("SdkVersion", "121.0.4887.0");
+            request.addHeader("SdkVersion", "121.0.4923.0");
 
             String applicationName = this.client.getAppName();
 
