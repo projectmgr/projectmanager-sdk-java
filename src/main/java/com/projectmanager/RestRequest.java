673e483e92c2065a06c23adaac099a1585b2e5d5
tspence <1933486+tspence@users.noreply.github.com>
2024-11-24 00:07:01 +0000

    # Patch notes for 122.0.216
    These patch notes summarize the changes from version 122.0.214.
    
    * Minor documentation changes, bugfixes, and performance improvements.

--- src/main/java/com/projectmanager/RestRequest.java
+++ src/main/java/com/projectmanager/RestRequest.java
@@ -148,7 +148,7 @@ public class RestRequest<@NotNull T> {
             }
 
             request.addHeader("SdkName", "Java");
-            request.addHeader("SdkVersion", "122.0.214.0");
+            request.addHeader("SdkVersion", "122.0.216.0");
 
             String applicationName = this.client.getAppName();
 
